e5be0126df66cf3b009416e79078552e9927e848
MatthewTanel <16mdt@queensu.ca>
2019-03-12 16:04:32 -0400

    added country

--- assignment1/Country.java
+++ assignment1/Country.java
@@ -1,5 +1,32 @@
 package assignment1;
 
-public class Country {
+public class Country extends Entity {
+private String capital;
+
+public Country(String name, Date birthDate) {
+	super(name, birthDate);
+	
+}
+
+public Country(Country country) {
+	super(country);
+}
+
+@Override
+public String entityType() {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+@Override
+public Entity clone() {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+public Country clone() {
+	
+}
+
 
 }
